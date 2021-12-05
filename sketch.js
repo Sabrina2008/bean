3c5ab8119d5757396840107860f7064aceaa2c65
Sabrina2008 <95552560+Sabrina2008@users.noreply.github.com>
2021-12-04 16:40:28 -0800

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,11 @@
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  //background(220);
+  fill(00);
+  line(mouseX, mouseY, 100, 100);
+  fill(220);
+  line(mouseX, mouseY);
+}
